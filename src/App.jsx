674bc11a9a31ba2437ef130df33232c96b0a23c4
Anurag Pramanik <p.anurag@outlook.com>
2023-03-12 21:51:20 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import DataDisplay from "./components/DataDisplay";
 
 const App = () => {
-  return <DataDisplay />;
+  return (
+    <>
+      <DataDisplay />
+    </>
+  );
 };
 
 export default App;
